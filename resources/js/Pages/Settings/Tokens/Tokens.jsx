954236462ec532f0c520aac92b9ef2600d606ff0
Moody <moody.boles1234@gmail.com>
2023-04-21 04:08:29 +0100

    Add success message

--- resources/js/Pages/Settings/Tokens/Tokens.jsx
+++ resources/js/Pages/Settings/Tokens/Tokens.jsx
@@ -16,6 +16,14 @@ export default function Tokens({ auth, tokens, newToken }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
+                    {newToken && (
+                        <div className="p-4 sm:p-8 sm:py-6 bg-white dark:bg-green-800 shadow sm:rounded-lg">
+                            <p className="text-gray-600 dark:text-gray-200">
+                                API key created successfully. Save it somewhere safe, as you won't be able to see it again.
+                            </p>
+                        </div>
+                    )}
+
                     <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                         <TokensList
                             tokens={tokens}
